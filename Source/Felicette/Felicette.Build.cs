b64ada59e7d15eb06a74968076aeb7f4c3c0f060
andrescrd <c.andresricardo@gmail.com>
2021-01-17 18:41:17 -0300

    adding niagara module

--- Source/Felicette/Felicette.Build.cs
+++ Source/Felicette/Felicette.Build.cs
@@ -8,6 +8,6 @@ public class Felicette : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","NavigationSystem", "AIModule", "Niagara" });
     }
 }
